89d56b3613864ca31b6e1b66389912c7525e42f3
joshuamccluskey <jpiff57@gmail.com>
2022-03-11 10:49:36 -0800

    All main fetures complete

--- src/main/java/com/joshuamccluskey/bcrypt/controller/UserController.java
+++ src/main/java/com/joshuamccluskey/bcrypt/controller/UserController.java
@@ -2,6 +2,8 @@ package com.joshuamccluskey.bcrypt.controller;
 
 import com.joshuamccluskey.bcrypt.model.AccessUser;
 import com.joshuamccluskey.bcrypt.repository.AccessUserRepository;
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,34 +11,41 @@ import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class UserController {
+    @Autowired
     AccessUserRepository accessUserRepository;
 
     @GetMapping("/")
-    public String getLogin() {
-        return "index.html";
+    public String getLoginPageToLogin() {
+        return "login.html";
 
     }
 
     @PostMapping("/login")
 
-    public RedirectView login(String username, String password) {
-        AccessUser newUser = new AccessUser(username, password);
-        accessUserRepository.save(newUser);
-        return new RedirectView("/");
+    public RedirectView loginUsernameAndPassword(String username, String password) {
+        AccessUser newUser = accessUserRepository.findByUsername(username);
+        if(newUser == null){
+            return new RedirectView("/login/");
+        }
+        boolean checksPasswordIsCorrect = BCrypt.checkpw(password, newUser.getPassword());
+
+        if(!checksPasswordIsCorrect) {
+            return new RedirectView("/login/");
+        } else {
+            return new RedirectView("/main/" + username);
+        }
     }
 
-    @GetMapping("/createaccount")
-    public String getCreateAccount() {
-        return "create-account.html";
+    @GetMapping("/signup")
+    public String getSignUpFormForAcccountCreation() {
+        return "signup.html";
     }
 
-    @PostMapping("/createaccount")
-    public RedirectView createaccount(String username, String password) {
-//        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());  // use default of 10
-        AccessUser newUser = new AccessUser(username, password);
+    @PostMapping("/signup")
+    public RedirectView createAccountToAccessSite(String username, String password) {
+        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+        AccessUser newUser = new AccessUser(username, hashedPassword);
         accessUserRepository.save(newUser);
-
-
         return new RedirectView("/");
 
     }
